4b59a7b5f2d72584682edacbd3f464bd149c28a1
GiuseppeGiorgi <giuseppe.giorgi1987@gmail.com>
2016-12-21 11:29:23 +0100

    Update Log.java

--- src/main/java/report/Log.java
+++ src/main/java/report/Log.java
@@ -2,6 +2,10 @@ package report;
 
 import javaCrawler.Record;
 
+/**
+ * @author Giuseppe Giorgi <giuseppe.giorgi1987@gmail.com>
+ */
+
 public interface Log <T extends Record> {
 
 	public void writeLog(T record);
