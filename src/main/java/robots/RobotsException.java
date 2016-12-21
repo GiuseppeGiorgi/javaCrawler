a448b60d1ef9ca8447d71bda24f9598c3a06a1e1
GiuseppeGiorgi <giuseppe.giorgi1987@gmail.com>
2016-12-21 11:30:55 +0100

    Update RobotsException.java

--- src/main/java/robots/RobotsException.java
+++ src/main/java/robots/RobotsException.java
@@ -1,5 +1,9 @@
 package robots;
 
+/**
+ * @author Giuseppe Giorgi <giuseppe.giorgi1987@gmail.com>
+ */
+
 public class RobotsException extends Exception{
 	
 	public RobotsException(String message){
